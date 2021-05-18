3b8879bacae389fed98bb94260b49dc764778b71
Aakash-Panchal <akashpanchal7000@gmail.com>
2021-05-18 17:36:09 +0000

    Revert "RMX1831: update blobs from C.23"
    This reverts commit 0ca9c5f57c7b593f8a3a81aa4601e49e7b16365d.

--- RMX1831-vendor.mk
+++ RMX1831-vendor.mk
@@ -7,7 +7,4 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1831/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
     vendor/realme/RMX1831/proprietary/lib/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsysenv_system.so \
     vendor/realme/RMX1831/proprietary/lib64/libmtk-ril.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmtk-ril.so \
-    vendor/realme/RMX1831/proprietary/lib64/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsysenv_system.so \
-    vendor/realme/RMX1831/proprietary/lib64/android.hardware.health@2.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/android.hardware.health@2.0.so \
-    vendor/realme/RMX1831/proprietary/lib64/libsuspend.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsuspend.so \
-    vendor/realme/RMX1831/proprietary/lib/libsuspend.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsuspend.so
+    vendor/realme/RMX1831/proprietary/lib64/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsysenv_system.so 
